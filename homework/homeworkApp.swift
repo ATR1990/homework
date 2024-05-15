350111b94e02adede1386a0949fb8412d4fc56d4
AltynbekovT <tauyekelaltynbekov@gmail.com>
2024-05-15 11:52:18 +0500

    Initial Commit

---
+++ homework/homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeworkApp.swift
+//  homework
+//
+//  Created by Тауекел on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
